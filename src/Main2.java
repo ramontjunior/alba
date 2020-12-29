24e96620df393e437f362d0c8c82118c9bc5bff3
Ramont Júnior <ramontjunior@hotmail.com>
2020-12-29 14:37:10 -0300

    Main de Teste

---
+++ src/Main2.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+
+public class Main2 {
+    public static void main(String[] args) {
+        
+        HashSet<String> set = new HashSet<>();
+        set.add("001");
+        set.add("001");
+        set.add("001");
+        set.add("001");
+        set.add("001");
+        set.add("001");
+        System.out.println(set.size());
+    }
+}
